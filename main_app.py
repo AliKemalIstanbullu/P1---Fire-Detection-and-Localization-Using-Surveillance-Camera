1e03b03468f023489e862443d91084111c3ea6f8
Andre <andre.dallaire@uoit.net>
2023-11-08 16:06:01 -0500

    update backend

--- main_app.py
+++ main_app.py
@@ -1,32 +1,24 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, request, render_template
+import cv2
+import torch
+import numpy as np
 
-
-UPLOAD_FOLDER = '\\'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+from fileinput import filename 
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
-    return render_template('index.html')
+   if request.method == 'POST':   
+        f = request.files['file'] 
+        f.filename = "test.jpg"
+        f.save(f.filename) 
+        image = cv2.imread("test.jpg")
+        results = model(image)
+        result_image = np.squeeze(results.render())
+        cv2.imwrite("result.jpg", result_image)
+        return render_template('index.html')
+   else:
+       return render_template('index.html')
