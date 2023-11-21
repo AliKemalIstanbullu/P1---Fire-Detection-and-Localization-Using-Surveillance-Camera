5e985f9bdbf0998fe3dccfc2f8968e4e2783cada
Andre Dallaire <adallaire@live.com>
2023-11-21 18:26:26 -0500

    added array of files added

--- main_app.py
+++ main_app.py
@@ -8,12 +8,15 @@ import numpy as np
 from fileinput import filename 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 
+videos = []
+
 app = Flask(__name__, '/static')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':   
-        f = request.files['filename'] 
+        f = request.files['filename']
+        videos.append(f.filename) 
         f.filename = "static\\test.jpg"
         f.save(f.filename) 
         image = cv2.imread("static\\test.jpg")
@@ -21,6 +24,6 @@ def upload_file():
         result_image = np.squeeze(results.render())
         cv2.imwrite("static\\result.jpg", result_image)
         #return "test"
-        return render_template('index.html')
+        return render_template('index.html', videos=videos)
    else:
        return render_template('index.html')
