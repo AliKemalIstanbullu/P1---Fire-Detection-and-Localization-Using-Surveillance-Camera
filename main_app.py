7f198eb7ecde563840b2a651d5b93a51fcd46998
Andre Dallaire <adallaire@live.com>
2023-12-06 11:19:24 -0500

    Added video processing

--- main_app.py
+++ main_app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template
 import cv2
 import torch
 import numpy as np
+import time
 
 
 from fileinput import filename 
@@ -14,16 +15,36 @@ app = Flask(__name__, '/static')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':   
-        f = request.files['filename']
-        videos.append(f.filename) 
-        f.filename = "static\\test.jpg"
-        f.save(f.filename) 
-        image = cv2.imread("static\\test.jpg")
-        results = model(image)
-        result_image = np.squeeze(results.render())
-        cv2.imwrite("static\\result.jpg", result_image)
-        #return "test"
-        return render_template('index.html', videos=videos)
-   else:
-       return render_template('index.html')
+     if request.method == 'POST':   
+          f = request.files['filename']
+          videos.append(f.filename) 
+          #f.filename = "static\\test.jpg"
+          f.save(f.filename) 
+          video = cv2.VideoCapture(f.filename)
+          while (True):
+               time.sleep(5) # take schreenshot every 5 seconds
+               # reading from frame
+               ret, frame = video.read()
+
+               if ret:
+                    # if video is still left continue creating images
+                    name = "static\\test.jpg"
+
+                    # writing the extracted images
+                    cv2.imwrite(name, frame)
+                    results = model(frame)
+                    result_image = np.squeeze(results.render())
+                    cv2.imwrite("static\\result.jpg", result_image)
+
+               else:
+                    break
+
+          # Release all space and windows once done
+          video.release()
+          cv2.destroyAllWindows()
+        
+        
+          #return "test"
+          return render_template('index.html', videos=videos)
+     else:
+          return render_template('index.html')
